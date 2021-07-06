3117696929987a44a794759b95355bd12ebd4893
Abdeljalil Boudjourfa <jalilboudjourfa@gmail.com>
2021-07-06 03:16:13 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import TourList from './components/tourList';
+function App() {
+  return (
+    <main>
+    <Navbar />
+    <TourList />
+  </main>);
+}
+
+export default App;
